cf069f05df03ff420f1042fdd9b1865734e80d48
Markos <markosmk@me.com>
2022-02-26 22:08:37 -0300

    update: added jwt secret config

---
+++ src/config/index.js
@@ -0,0 +1,14 @@
+const config = {
+  db: {
+    name: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+  },
+  jwt_secret: process.env.JWT_SECRET,
+  app_host: process.env.APP_HOST || 'localhost',
+  app_port: process.env.APP_PORT || 8000,
+};
+module.exports = config;

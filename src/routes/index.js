2d91546c61d0a2a7b0231797b3af81c72dba419a
Markos <markosmk@me.com>
2022-02-27 09:57:24 -0300

    update: added userRoute

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,7 +9,7 @@ const movieRoute = require('./movieRoute');
 const validRequest = require('../middlewares/validRequest');
 
 router.use('/auth', authRoute);
-router.use('/user', validRequest, userRoute);
+router.use('/users', validRequest, userRoute);
 router.use('/characters', validRequest, characterRoute);
 router.use('/movies', validRequest, movieRoute);
 

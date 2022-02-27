6fc9fce91521077cd715a84ab8e4fa7e9b75254a
Markos <markosmk@me.com>
2022-02-27 10:01:14 -0300

    update: added jsdoc in each route data

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -9,10 +9,71 @@ const {
   renewPass,
 } = require('../controllers/authController');
 
+/**
+ * POST /api/v1/auth/login
+ * @summary Inicia sesion de usuario
+ * @tags auth
+ * @param {object} request.body.required - Datos Necesarios
+ * @return {object} 200 - success response
+ * @example request - example payload
+ * {
+ *   "email": "email@email.com",
+ *   "password": "123456"
+ * }
+ * @example response - 200 - success response example
+ * {
+ *   "message": "Ingresado Correctamente",
+ *   "user": {},
+ *   "token": "eyJhbGciOiJIUzI1NiIsInR5cCI..."
+ * }
+ */
 router.post('/login', login);
+
+/**
+ * POST /api/v1/auth/register
+ * @summary Registra un nuevo usuario
+ * @tags auth
+ * @param {object} request.body.required - name param description
+ * @return {object} 201 - success response
+ * @example request - example payload
+ * {
+ *   "name": "Nombre",
+ *   "email": "email@email.com",
+ *   "password": "123456"
+ * }
+ * @example response - 201 - success response example
+ * {
+ *   "message": "Registrado Correctamente",
+ *   "user": {}
+ * }
+ */
 router.post('/register', register);
+
+/**
+ * GET /api/v1/auth/verify/{token}
+ * @summary Verifica Email de Usuario
+ * @tags auth
+ * @param {string} token.path - token valido
+ * @return {object} 200 - success response
+ */
 router.get('/verify/:token', verifyEmail);
+
+/**
+ * POST /api/v1/auth/forgetpw
+ * @summary Registra un nuevo usuario
+ * @tags auth
+ * @param {object} request.body.required - name param description
+ * @return {object} 201 - success response
+ */
 router.post('/forgetpw', forgetPass);
+
+/**
+ * POST /api/v1/auth/lastpw/{token}
+ * @summary Registra un nuevo usuario
+ * @tags auth
+ * @param {object} request.body.required - name param description
+ * @return {object} 201 - success response
+ */
 router.post('/lastpw/:token', renewPass);
 
 module.exports = router;
--- src/routes/movieRoute.js
+++ src/routes/movieRoute.js
@@ -8,12 +8,75 @@ const {
   updateMovie,
   deleteMovie,
 } = require('../controllers/movieController');
+/**
+ * Modelo Esquema de Generos
+ * @typedef {object} Gender
+ * @property {string} name.required - Nombre de Genero
+ * @property {string} image.required - Imagen de este genero
+ */
+/**
+ * Modelo Esquema de Peliculas/Series
+ * @typedef {object} Movie
+ * @property {string} name.required - Titulo de pelicula/serie
+ * @property {string} score.required - Puntuacion del 1 al 5
+ * @property {string} image.required - Poster de pelicula
+ * @property {string} dateCreated.required - Fecha estreno de pelicula
+ * @property {integer} GenderId.required - Id de Genero asociado a la pelicula
+ * @property {array<Character>} characters - Personajes asociados
+ * @property {Gender} Gender - Info Genero asociado
+ */
 
+/**
+ * GET /api/v1/movies
+ * @summary Lista todas las peliculas, y permite filtrar por genero, ordernar por fecha, y buscar por nombre.
+ * @tags movies
+ * @security Bearer
+ * @param {string} name.query - Buscar por titulo de pelicula
+ * @param {integer} genre.query - Filtro por `Id` genero de pelicula
+ * @param {string} order.query - Order por fecha de creacion - enum:ASC,DESC
+ * @return {object} 200 - success response
+ */
 router.get('/', getMovies);
-// CRUD
+
+/**
+ * GET /api/v1/movies/{id}
+ * @summary Recibe los detalles de una determinada pelicula.
+ * @tags movies
+ * @security Bearer
+ * @param {string} id.path.required - identificador de pelicula
+ * @return {object} 200 - success response
+ */
 router.get('/:id', getOneMovie);
+
+/**
+ * POST /api/v1/movies/
+ * @summary Crea una nueva pelicula/serie.
+ * @tags movies
+ * @security Bearer
+ * @param {object} request.body.required - Datos Necesarios
+ * @return {object} 201 - success response
+ */
 router.post('/', createMovie);
+
+/**
+ * PUT /api/v1/movies/{id}
+ * @summary Actualiza datos de una pelicula/serie.
+ * @tags movies
+ * @security Bearer
+ * @param {string} id.path.required - identificador de pelicula
+ * @param {object} request.body.required - Datos Necesarios
+ * @return {object} 200 - success response
+ */
 router.put('/:id', updateMovie);
+
+/**
+ * DELETE /api/v1/movies/{id}
+ * @summary Elimina una determinada pelicula/serie.
+ * @tags movies
+ * @security Bearer
+ * @param {string} id.path.required - identificador de pelicula
+ * @return {object} 200 - success response
+ */
 router.delete('/:id', deleteMovie);
 
 module.exports = router;
